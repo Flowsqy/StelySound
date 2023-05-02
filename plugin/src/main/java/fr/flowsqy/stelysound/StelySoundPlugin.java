ebc53a93bb28f0f35f455661265ddb0d20bf7df6
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2023-05-02 13:44:42 +0200

    Add gradle and plugin main class

---
+++ plugin/src/main/java/fr/flowsqy/stelysound/StelySoundPlugin.java
@@ -0,0 +1,11 @@
+package fr.flowsqy.stelysound;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class StelySoundPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
